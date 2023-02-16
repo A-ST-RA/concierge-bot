e7f114bd7b1416d69f2878654d3b20d975998447
Vladislav Bachmackiy <astrax200469@gmail.com>
2023-02-16 22:22:39 +0300

    feat(tg): wip

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 import { Telegraf } from 'telegraf';
+import { Markup } from 'telegraf';
 import AppConfig from './config';
 
 const bot = new Telegraf(AppConfig.botToken);
 bot.hears('test', (ctx) => ctx.reply('test success'));
+bot.start(
+	(ctx) => 
+		ctx.reply(
+			'Keyboard sended',
+			MarkupgreetingData.keyboard(
+				[
+					Markup.button.callback('Check', 'check_it')
+				]
+			).resize()
+		)
+);
+
+bot.hears('Check', (ctx) => {console.log(ctx)})
 
 bot.launch();
